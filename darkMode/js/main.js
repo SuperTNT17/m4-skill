74d32bb2e45cbe2c8d5159a794117ccb28ef7bbb
Robin Lemmers <38210@ma-web.nl>
2024-06-13 09:50:57 +0200

    darkmode af

--- darkMode/js/main.js
+++ darkMode/js/main.js
@@ -1,1 +1,16 @@
-console.log("de javascript werkt!");
+const rootElement = document.querySelector(":root");
+const modeSwitch = document.getElementById("mode");
+
+
+modeSwitch.checked = false;
+modeSwitch.onchange = function () {
+    if (modeSwitch.checked === true) {
+        rootElement.style.setProperty("--background-color", "white");
+        rootElement.style.setProperty("--font-color", "black");
+        rootElement.style.setProperty("--produt-border-color", "black)");
+        return;
+    }
+    rootElement.style.setProperty("--background-color", "#212730");
+    rootElement.style.setProperty("--font-color", "#d4d4d4");
+    rootElement.style.setProperty("--product-border-color", "rgba(212,212,212,0.4)");
+}
